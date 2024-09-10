1684a01aa1f1df4250bf29957149cebfd3ea47b8
SyedFazal110 <syedfazal808@gmail.com>
2024-09-10 22:22:50 +0500

    Still Pending

--- Router Dom/src/pages/home.jsx
+++ Router Dom/src/pages/home.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const [data , setData] = useState(null);
+
+  const navigate = useNavigate()
+
+  useEffect( () => {
+    axios('https://jsonplaceholder.typicode.com/users')
+    .then( res => res.json())
+    .then( (res) => {
+      console.log(res)
+      setData(res)
+    })
+    .catch( (error) => {
+      console.log(error);
+      
+    })
+  } , [])
+
+
   return (
     <div>Home</div>
   )
